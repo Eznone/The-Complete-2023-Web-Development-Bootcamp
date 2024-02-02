77321a03fb3189a7dea0d86e9b67dd162dfbb6de
Eznone <enzotresmediano@gmail.com>
2024-02-02 12:11:59 -0300

    Finished 4.2

--- Section 25) EJS/4.2 Passing Data/index.js
+++ Section 25) EJS/4.2 Passing Data/index.js
@@ -6,9 +6,17 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  let size = req.body["fName"].length + req.body["lName"].length;
+  let data = {
+    size: size,
+  }
+  res.render("index.ejs", data);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
