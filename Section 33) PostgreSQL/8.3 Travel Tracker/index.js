816c6faee621a404373f835156a0898bd544e2d5
Eznone <enzotresmediano@gmail.com>
2024-03-04 16:12:27 -0300

    Finished with /add of the country site

--- Section 33) PostgreSQL/8.3 Travel Tracker/index.js
+++ Section 33) PostgreSQL/8.3 Travel Tracker/index.js
@@ -19,20 +19,40 @@ const db = new pg.Client ({
 
 db.connect();
 
-app.get("/", async (req, res) => {
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let visited_countries = [];
   result.rows.forEach((country) => {
     visited_countries.push(country.country_code);
   });
+  return visited_countries;
+}
+
+async function addCountry(country) {
+  const newCountry = await db.query("SELECT * FROM countries WHERE country_code='" + country.toUpperCase() + "'" );
+  if (newCountry.rows.length !== 0) {
+    const countryCode = newCountry.rows[0]["country_code"];
+    console.log(countryCode);
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+  }
+}
+
+
+app.get("/", async (req, res) => {
+  let visited_countries = await checkVisisted();
   console.log(visited_countries);
   let data = {
     countries: visited_countries,
     total: visited_countries.length
   };
-  db.end();
   res.render("./index.ejs", data);
 });
+
+app.post("/add", (req, res) => {
+  addCountry(req.body.country);
+  res.redirect("/");
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
