4cf735a4266f0240dfbc64019dcd3c021fe48df8
Eznone <enzotresmediano@gmail.com>
2024-03-17 19:01:10 -0300

    Encyption 1 began

---
+++ Section 35) Authentication and Security/9.1 Authentecation Lv. 1/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {});
+
+app.post("/login", async (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
