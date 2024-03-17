62f3df4fec70059856cf6af0aea8f1c20f349039
Eznone <enzotresmediano@gmail.com>
2024-03-17 14:11:21 -0300

    Finished PostgreSQL

--- Section 33) PostgreSQL/8.6 Permalist Project/index.js
+++ Section 33) PostgreSQL/8.6 Permalist Project/index.js
@@ -1,33 +1,63 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  host: "localhost",
+  user: "postgres",
+  password: "123456",
+  port: 5432,
+  database: "permalist"
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+var items = [];
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
-
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+app.post("/add", async (req, res) => {
+  try {
+    const itemToAdd = req.body.newItem;
+    await db.query("INSERT INTO items (title) VALUES($1)", [itemToAdd]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  try {
+    const itemToUpdate = req.body;
+    await db.query("UPDATE items SET title = $1 WHERE id = $2", [itemToUpdate.updatedItemTitle, itemToUpdate.updatedItemId]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [req.body.deleteItemId]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
