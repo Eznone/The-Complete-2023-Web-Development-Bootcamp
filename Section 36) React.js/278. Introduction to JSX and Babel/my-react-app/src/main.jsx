e560c77b0e4dfcbd69bbc76c8a336b38cbf168fa
Eznone <enzotresmediano@gmail.com>
2024-04-21 15:09:13 -0300

    Finished tutorial to basic JSX

---
+++ Section 36) React.js/278. Introduction to JSX and Babel/my-react-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragrpah.</p>
+  </div>,
+  document.getElementById("root")
+);
